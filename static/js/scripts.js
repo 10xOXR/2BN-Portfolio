c34ff51a50eb139aad0262688b12d606484d19a6
10xOXR <10xoxr@gmail.com>
2019-10-10 21:13:55 +0100

    Apply additional styling and placeholders
    Begin applying styling to each section and added placeholder for
    the hero image above navbar. Navbar now sticky when it reaches
    top of viewport. Smooth scrolling added.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $(".sidenav").sidenav({edge:'right'});
+
+    $("a").click(function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top - 150
+            }, 100);
+        }
+    });
+
+    $(".sidenav").sidenav({edge: "right"});
+
 });
